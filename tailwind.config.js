d2f5ea3b2e2d95c8b81fd6baac6da544dc3b32c1
Luke <luke@luke.work>
2020-11-30 19:26:43 -0600

    added fadeInDown animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 module.exports = {
-    theme: {
-        
-      extend: {
-        colors: {
-          change: "transparent"
-        }
-      }
-    },
-    variants: {},
-    plugins: [],
-    darkMode: 'media',
-  }
+  theme: {
+      
+    extend: {
+      colors: {
+        change: "transparent"
+      },
+        animation: {
+          fadeInDown: "fadeInDown 1s ease-in-out forwards"
+        },
+        keyframes: {
+          fadeInDown: {
+            "0%": { opacity: 0, transform: 'translateY(-50%)'},
+            "100%": { opacity: 1, transform: 'translateY(0)'}
+          }
+        },
+    }
+  },
+  variants: {},
+  plugins: [],
+  darkMode: 'media',
+}
